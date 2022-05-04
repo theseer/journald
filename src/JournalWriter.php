bbd1b871a8d9f298379d2c830285cb4b06d1c782
Arne Blankerts <Arne@Blankerts.de>
2022-05-04 10:10:56 +0200

    Add Error handling to socket interactions

--- src/JournalWriter.php
+++ src/JournalWriter.php
@@ -1,19 +1,43 @@
 <?php declare(strict_types=1);
 namespace theseer\journald;
 
+use function pack;
+use function socket_clear_error;
+use function socket_close;
+use function socket_connect;
+use function socket_create;
+use function socket_send;
+use function sprintf;
+use function str_contains;
+use function strlen;
+
 final class JournalWriter {
 
     public function __construct(
        private SocketPath $socketPath
     ) {}
 
+    /**
+     * @throws JournalWriterException
+     */
     public function write(JournalEntry $entry): void {
         $sock = socket_create(AF_UNIX, SOCK_DGRAM, 0);
-        socket_connect($sock, $this->socketPath->asString());
+        socket_clear_error($sock);
+
+        if (!socket_connect($sock, $this->socketPath->asString())) {
+            $error = \socket_last_error($sock);
+            throw new JournalWriterException(
+                sprintf(
+                    'Failed to connect to journald: %s (error %d)',
+                    \socket_strerror($error),
+                    $error,
+                )
+            );
+        }
 
         $payload = '';
         foreach($entry as $key => $value) {
-            if (\str_contains($value, "\n")) {
+            if (str_contains($value, "\n")) {
                 $payload .= sprintf(
                     "%s\n%s%s\n",
                     $key,
@@ -29,7 +53,30 @@ final class JournalWriter {
 
         $len = strlen($payload);
 
-        socket_send($sock, $payload, $len, 0);
+        $res = socket_send($sock, $payload, $len, 0);
+        if ($res === false || $res !== $len) {
+            $error = \socket_last_error($sock);
+            if ($error !== 0) {
+                throw new JournalWriterException(
+                    sprintf(
+                        'Failed to write log entry: %s (error %d - wrote %d of %d bytes)',
+                        \socket_strerror($error),
+                        $error,
+                        (int)$res,
+                        $len
+                    )
+                );
+            }
+
+            throw new JournalWriterException(
+                sprintf(
+                    'Failed to write to journald socket - no error code available (%d of %d bytes written',
+                    (int)$res,
+                    $len
+                )
+            );
+        }
+
         socket_close($sock);
     }
 }
